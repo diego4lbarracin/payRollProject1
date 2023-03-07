5ef3f7757e1b899453ea96f74bda9c0ec0b3c6dd
DiegoAlbarracin <diegoalbarracin0405@gmail.com>
2023-03-07 09:49:54 -0500

    First Commit

---
+++ finalProject.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+	
+	//David
+	
+	
+	
+	
+	
+}
+
+//Funciones----------------------------------
+
+void AdicionEmpleado(){
+	//Santiago
+}
+
+void ActualizarInfo(){
+	//Santiago	
+}
+
+void Reporte(){	
+	fstream diego;
+	diego.open("hola.txt");
+}
+
+void ConsultarInfo(){
+//David	
+}
+
+void CrearTabla(){	
+//Diego
+}
