08cd3f6ab893ab33bf58e99ac607744508f249e7
DiegoAlbarracin <diegoalbarracin0405@gmail.com>
2023-03-13 21:14:54 -0500

    Inicializando la estructura

--- finalProject.cpp
+++ finalProject.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
-#include <string.h>
-#include <fstream>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+struct infoEmpleado
+{	
+	char tipoDoc[2]; //CC, Cedula Ciudadania, CE, Cedula Extranjeria
+	char numDoc[16];
+	char nombre[20];
+	char apellido[20];
+	char nivelEducativo[2];//EB, Educacion Basica, EM, Educacion Media, ES, Educacion Superior.
+	int anosExperiencia;
+	int numCertificaciones;
+};
+
 
 int main(){
 	
